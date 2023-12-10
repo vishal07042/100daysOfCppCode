d2afeb1b472f685fa1a5bc162f5e7cc632bd3fe8
vishal mehta <professionalprovishal@gmail.com>
2023-12-10 16:13:25 +0530

    day 2  3 ghante se bhi zyada ho gaye ek swall ni hua

---
+++ spiralMatrix.cpp
@@ -0,0 +1,21 @@
+vector<int> spiralOrder(vector<vector<int>> &matrix)
+{
+
+    int row = matrix.size();
+    int col = matrix[0].size();
+
+    int count = 0;
+    int total = row * col;
+
+    int startingRow = 0;
+    int startingcol = 0;
+
+    int endingRow = row - 1;
+    int endingCol = col - 1;
+
+    while (count < total)
+    {
+
+       
+    }
+}
