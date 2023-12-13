02db1af0fe0030d1440ae98eceb5561c6c31da8f
vishal mehta <professionalprovishal@gmail.com>
2023-12-13 21:40:10 +0530

    init

---
+++ bb1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{     int total=0;
+int num;
+    for (int i = 1; i <=3 ; i++)
+    {
+
+    // 4^3= 64
+        num=2;
+       
+         num=num+num*i;
+
+         
+       
+        /* code */
+    }
+    cout<<num/10;
+     cout<<total;
+    return 0;
+}
---
+++ neww.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int d=4>>2;
+    cout<<d;
+    return 0;
+}
